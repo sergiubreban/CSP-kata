32a52e198a266e82472c1a75baabe0b847be210c
Sergiu Breban <sergiu.breban@cj003452mc.esp.local>
2018-11-23 16:14:02 +0200

    removed csp middle

--- index.js
+++ index.js
@@ -4,27 +4,6 @@ const bodyParser = require('body-parser')
 
 const app = express()
 const port = 3000;
-let sources = {
-    'default-src': ['\'self\''],
-    'script-src': ['\'self\''],
-    'img-src': ['\'self\'', "data:"],
-    'media-src': ['\'self\''],
-    'style-src': ['\'self\'', "'unsafe-inline'"], 
-    'font-src': ['\'self\''],
-    'report-uri': ['/report-violation']
-}
-
-let csp = Object.keys(sources).map(function (key) {
-    return `${key} ${sources[key].join(' ')};`
-})
-
-const CSPMiddleware =  function (req, res, next) {
-    res.setHeader('Content-Security-Policy', csp.join(' '))
-    next()
-}
-
-app.use(CSPMiddleware);
-
 
 app.use(bodyParser.json({
     type: ['json', 'application/csp-report']
